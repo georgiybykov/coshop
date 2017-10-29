abfc2e8f1c12801f8069e0ca2ff0f71f313299fe
georgiybykov <zhora.bykov@gmail.com>
2017-10-29 23:10:41 +0300

    Uploaded data to db; Rewrote index.html.erb; added stylesheet application.css and uploaded it to the app via application.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+Product.create!(title: 'Peaceful warrior',
+  description:
+    %{ <p>
+        Warrior is the person you should to become. Because you will achive all your goals and feel quite inside.
+    </p>},
+  image_url: 'peaceful_warrior.jpg',
+  price: 29.95)
