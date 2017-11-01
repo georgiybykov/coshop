2aeb86ff755ff3326f613f266b4b22fb769a4339
georgiybykov <zhora.bykov@gmail.com>
2017-11-02 00:20:18 +0300

    Finished block tests in product_test.rb; Changed some Title validations like validates_length_of; Made fixed test template for product info

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test 'product attributes must not be empty' do
     product = Product.new
     assert product.invalid?
@@ -15,7 +17,7 @@ class ProductTest < ActiveSupport::TestCase
                           description: "yyy",
                           image_url: "zzz.jpg")
     product.price = -1
-    assert.product.invalid?
+    assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
 
     product.errors[:price]
@@ -25,7 +27,7 @@ class ProductTest < ActiveSupport::TestCase
     assert_equal ["must be greater than or equal to 0.01"],
       product.errors[:price]
     product.price = 1
-    ass product.invalid?
+    assert product.valid?
   end
 
   def new_product(image_url)
@@ -47,5 +49,25 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
+  end
+
+  test 'product is not valid without a unique title - i18n' do
+    product = Product.new(title: products(:book).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+
+    assert product.invalid?
+
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
+
+  test 'product title must be at least ten characters long' do
+    product = Product.new(title: "Too short",
+                          description: "yyy",
+                          price: 1,
+                          image_url: "zzz.jpg")
+
+    assert product.invalid?
   end
 end
